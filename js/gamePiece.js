b3cc7cc67a86443a864d81a1a78a8a85b294cc0f
JMCassoli <jcassoli@alumnos.exa.unicen.edu.ar>
2021-10-24 04:47:50 -0300

    Se agrego la funcionalidad cambiar ficha y demas

--- js/gamePiece.js
+++ js/gamePiece.js
@@ -30,28 +30,18 @@ class gamePiece{
 
     draw() { //Carga y dibuja todas las imagenes en la matriz
 
-        let posX=this.getPosX();
+        let posX=this.getPosX()-2;
         let posY=this.getPosY();
         let size=this.size;
         switch (this.getPlayer()) {
-            case 1:
-                let img1=new Image();
-                img1.src=document.getElementById("fichaRoja").src;
-                //img1.onload = function(){
-                ctx.drawImage(img1,posX+9,posY+10,size,size);
-                //}
-                
+            case 1:     
+                ctx.drawImage(token1,posX,posY,80,80);             
                 break;
         
             case 2:
-                let img2=document.createElement('img');
-                img2.src=document.getElementById("fichaVerde").src;
-                //img2.onload = function(){
-                    ctx.drawImage(img2,posX,posY,80,80);// acomodo los valores para que calse bien
-                                                                      // hay que corregir  
-                //}
-                
+                    ctx.drawImage(token2,posX,posY,80,80);                
                 break;
+
             default:
                 break;
         }
--- js/main.js
+++ js/main.js
@@ -11,33 +11,57 @@ let win=false;
 let turn=0;
 let player= (turn%2)+1;
 let mode=4;
+let token1 = document.getElementById("FichaAzul");
+let token2 = document.getElementById("FichaRoja");
 let clock = document.getElementById("time");
-let btnStart = document.getElementById("start").addEventListener("click",start);
-let btnRestart = document.getElementById("restart").addEventListener("click", reloadPage);
-let btnMode4 = document.getElementById("mode4").addEventListener("click", function () {mode=4});
-let btnMode5 = document.getElementById("mode5").addEventListener("click", function () {mode=5});
-let btnMode6 = document.getElementById("mode6").addEventListener("click", function () {mode=6});
+let btnStart = document.getElementById("start");
+btnStart.addEventListener("click",start);
+let btnRestart = document.getElementById("restart");
+btnRestart.addEventListener("click", reloadPage);
+let infoMode = document.getElementById("infoMode");
+let btnMode4 = document.getElementById("mode4");
+btnMode4.addEventListener("click", function () {mode=4; updateInfoMode()});
+let btnMode5 = document.getElementById("mode5");
+btnMode5.addEventListener("click", function () {mode=5; updateInfoMode()});
+let btnMode6 = document.getElementById("mode6");
+btnMode6.addEventListener("click", function () {mode=6; updateInfoMode()});
+let slcGreen = document.getElementById("selectGreen");
+slcGreen.addEventListener("click", function () {token1=document.getElementById("FichaVerde");});
+let slcBlue = document.getElementById("selectBlue");
+slcBlue.addEventListener("click", function () {token1=document.getElementById("FichaAzul");});
+let slcRed = document.getElementById("selectRed");
+slcRed.addEventListener("click", function () {token2=document.getElementById("FichaRoja1");});
+let slcYellow = document.getElementById("selectYellow");
+slcYellow.addEventListener("click", function () {token2=document.getElementById("FichaAmarilla");});
+
 
 function start(){
+    btnMode4.classList.toggle("nonClickable");
+    btnMode5.classList.toggle("nonClickable");
+    btnMode6.classList.toggle("nonClickable");
+    slcGreen.classList.toggle("nonClickable");
+    slcBlue.classList.toggle("nonClickable");
+    slcYellow.classList.toggle("nonClickable");
+    slcRed.classList.toggle("nonClickable");
     switch (mode) {
         case 6:
             columns=9;
             rows=8;
-            piecesGamer(1,765,45,document.getElementById("fichaRoja"),35); // cargo en un arreglo las fichas del jugador 1
-            piecesGamer(2,765,1280,document.getElementById("fichaVerde"),42); 
+            piecesGamer(1,765,45,token1,42); // cargo en un arreglo las fichas del jugador 1
+            piecesGamer(2,765,1280,token2,42); 
             break;
     
         case 5:
             columns=8;
             rows=7;
-            piecesGamer(1,685,45,document.getElementById("fichaRoja"),35); // cargo en un arreglo las fichas del jugador 1
-            piecesGamer(2,685,1280,document.getElementById("fichaVerde"),42); 
+            piecesGamer(1,685,45,token1,42); // cargo en un arreglo las fichas del jugador 1
+            piecesGamer(2,685,1280,token2,42); 
             break;
         default:
             columns=7;
             rows=6;
-            piecesGamer(1,605,45,document.getElementById("fichaRoja"),35); // cargo en un arreglo las fichas del jugador 1
-            piecesGamer(2,605,1080,document.getElementById("fichaVerde"),42);   
+            piecesGamer(1,605,45,token1,42); // cargo en un arreglo las fichas del jugador 1
+            piecesGamer(2,605,1080,token2,42);   
         break;
     }
    
@@ -45,6 +69,8 @@ function start(){
     game=new gameBoard(columns,rows);
     game.buildBoard(); 
     drawFig();
+    btnStart.className = "hidden";
+    btnRestart.classList.remove("hidden");
     document.getElementById("turn").toggleAttribute("class");
     document.getElementById("timer").toggleAttribute("class");
     timer=setInterval(updateTime,1000);
@@ -66,6 +92,10 @@ function updateTime() {
     else{
         clock.innerHTML = Math.floor(seconds / 60) + ":" + seconds % 60;
     }
+}
+
+function updateInfoMode(){
+    infoMode.innerHTML= "Modo "+ mode  +" en linea:"
 }
 
 // let game=new gameBoard(7,6);
